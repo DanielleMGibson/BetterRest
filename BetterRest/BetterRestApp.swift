031c8b315b550b64d464ddaf862ccaa21c7bad2d
Danielle Gibson <peyton.gibson@stu.barren.kyschools.us>
2024-11-15 08:55:15 -0600

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Student on 11/15/24.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
